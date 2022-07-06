1e4d4de75e24c915a945260483f832c82cfd04e2
qasim <qasim@rayqube.com>
2022-07-06 18:02:26 +0400

    w

--- app/Http/Controllers/select2DataController.php
+++ app/Http/Controllers/select2DataController.php
@@ -11,65 +11,83 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class select2DataController extends Controller
 {
-    public function downloadFile($table,$field,$id){
+    public function downloadFile($table, $field, $id)
+    {
         $table = Crypt::decrypt($table);
         $file = DB::table($table)->find($id);
-        return response()->download(public_path('uploads/'.$file->{$field}));
+        $str = str_replace(env('AWS_URL'),'',$file->{$field});
+        $str_arr = explode('/',$str);
+        $s = explode('.',$str_arr[2]);
+        $headers = [
+            'Content-Type' => 'application/'.$s[1],
+            'Content-Disposition' => 'attachment; filename="'.$str_arr[2].'"',
+        ];
+        return \Response::make(Storage::disk('s3')->get($str), 200, $headers);
+//        https://hyper-taskmanager.s3.ap-south-1.amazonaws.com/image/qOTnVjT4xcnNR5FB9txfoqUz6CewgFttdWnhOTVH.png
+//        return response()->download(public_path('uploads/' . $file->{$field}));
     }
+
+
     public function getClients(Request $request)
     {
         if ($request->has('search')) {
-            $data = Clients::select('id','name')->where('name','like','%'.$request->search.'%')->get();
+            $data = Clients::select('id', 'name')->where('name', 'like', '%' . $request->search . '%')->get();
         } else {
-            $data = Clients::select('id','name')->get();
+            $data = Clients::select('id', 'name')->get();
         }
         return response()->json($data);
     }
+
     public function getDepartments(Request $request)
     {
         if ($request->has('search')) {
-            $data = Departments::select('id','name')->where('name','like','%'.$request->search.'%')->get();
+            $data = Departments::select('id', 'name')->where('name', 'like', '%' . $request->search . '%')->get();
         } else {
-            $data = Departments::select('id','name')->get();
+            $data = Departments::select('id', 'name')->get();
         }
         return response()->json($data);
     }
+
     public function getLocations(Request $request)
     {
         if ($request->has('search')) {
-            $data = Locations::select('id','name')->where('name','like','%'.$request->search.'%')->get();
+            $data = Locations::select('id', 'name')->where('name', 'like', '%' . $request->search . '%')->get();
         } else {
-            $data = Locations::select('id','name')->get();
+            $data = Locations::select('id', 'name')->get();
         }
         return response()->json($data);
     }
-    public function getAssets(Request $request,$key = null)
+
+    public function getAssets(Request $request, $key = null)
     {
         if ($request->has('search')) {
-            $data = Assets::select('id','name','ref_no')->where('name','like','%'.$request->search.'%')/*->where('location',$key)*/->get();
+            $data = Assets::select('id', 'name', 'ref_no')->where('name', 'like', '%' . $request->search . '%')/*->where('location',$key)*/ ->get();
         } else {
-            $data = Assets::select('id','name','ref_no')/*->where('location',$key)*/->get();
+            $data = Assets::select('id', 'name', 'ref_no')/*->where('location',$key)*/ ->get();
         }
         return response()->json($data);
     }
+
     public function getCampaign(Request $request)
     {
         if ($request->has('search')) {
-            $data = Campaigns::select('id','name')->where('name','like','%'.$request->search.'%')->where('agency','like','%'.$request->search.'%')->where('brand','like','%'.$request->search.'%')->whereIn('status',['Active','Live'])->get();
+            $data = Campaigns::select('id', 'name')->where('name', 'like', '%' . $request->search . '%')->where('agency', 'like', '%' . $request->search . '%')->where('brand', 'like', '%' . $request->search . '%')->whereIn('status', ['Active', 'Live'])->get();
         } else {
-            $data = Campaigns::select('id','name')->whereIn('status',['Active','Live'])->get();
+            $data = Campaigns::select('id', 'name')->whereIn('status', ['Active', 'Live'])->get();
         }
         return response()->json($data);
     }
+
     public function getUser(Request $request)
     {
         if ($request->has('search')) {
-            $data = User::select('id','name')->where('name','like','%'.$request->search.'%')->where('status','Active')->get();
+            $data = User::select('id', 'name')->where('name', 'like', '%' . $request->search . '%')->where('status', 'Active')->get();
         } else {
-            $data = User::select('id','name')->where('status','Active')->get();
+            $data = User::select('id', 'name')->where('status', 'Active')->get();
         }
         return response()->json($data);
     }
