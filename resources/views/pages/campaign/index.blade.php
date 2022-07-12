2642bb0a33509165d2305c03d5956e144420c015
Syed <hasan@rayqube.com>
2022-07-12 17:32:30 +0400

    css added

--- resources/views/pages/campaign/index.blade.php
+++ resources/views/pages/campaign/index.blade.php
@@ -135,7 +135,7 @@
                   <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a href="#">Home</a></li>
                     <li class="breadcrumb-item active" aria-current="page">
-                      Library
+                      Calendar
                     </li>
                   </ol>
                 </nav>
@@ -316,16 +316,6 @@
     <script src="{{ asset('dist/js/jquery-ui.min.js')}}"></script>
     <!-- Bootstrap tether Core JavaScript -->
     <script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
-    <!-- slimscrollbar scrollbar JavaScript -->
-    <script src="{{ asset('assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js')}}"></script>
-    <script src="{{ asset('assets/extra-libs/sparkline/sparkline.js')}}"></script>
-    <!--Wave Effects -->
-    <script src="{{ asset('dist/js/waves.js')}}"></script>
-    <!--Menu sidebar -->
-    <script src="{{ asset('dist/js/sidebarmenu.js')}}"></script>
-    <!--Custom JavaScript -->
-    <script src="{{ asset('dist/js/custom.min.js')}}"></script>
-    <!-- this page js -->
     <script src="{{ asset('assets/libs/moment/min/moment.min.js')}}"></script>
     <script src="{{ asset('assets/libs/fullcalendar/dist/fullcalendar.min.js')}}"></script>
     <script src="{{ asset('dist/js/pages/calendar/cal-init.js')}}"></script>
